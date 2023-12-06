ae5c37a4249d0b2aa7eb640bd697e9215728a4cd
anne209 <152607616+anne209@users.noreply.github.com>
2023-12-06 22:26:51 +0100

    commit
    push

---
+++ Anleitung von Anne/Synchronisieren.vue
@@ -0,0 +1,20 @@
+Von local zu GitHub
+
+Dafür sorgen, dass es remote gitHub auf dem Laptop gibt "kiosk [gitHub]"
+File -> new window -> entweder open folder oder clone repository
+programmieren und speichern
+auf source control klicken
+es gibt einen reiter "changes", unter "changes" sieht man die Veränderungen, die man vorgenommen hat
+Die Veränderungen, die man committen will, auswählen und auf plus neben den Veränderungen klicken
+Es öffnet sich staged changes, bei denen die changes drin sind, die committet werden sollen
+bei message "commit" eingeben und auf "commit" drücken
+
+bei kiosk [GitHub] auf sync klicken, dann synchronisieren
+Alternativ auf Source Control klicken, dann auf Sync Changes (Pfeil nach unten)
+
+
+
+Von GitHub zu local
+Veränderungen von [GitHub] committen und pushen
+Auf dem local auf source control klicken
+Danach erscheint ein Button mit Sync Changes (Pfeil nach unten), darauf klicken
