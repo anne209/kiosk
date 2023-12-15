626842a14cf3f265d54ea46b820850a9b49a2909
haukur1710 <73662487+haukur1710@users.noreply.github.com>
2023-12-15 14:38:25 +0100

    commit

--- components/user.vue
+++ components/user.vue
@@ -1,74 +1,81 @@
 <script setup>
-  const props = defineProps(['user','PIN']          // hier kommt wird die pin als prop übergeben 
-    
-  ); 
-  const successMessage =ref('')
-  const data = ref({
-    enteredPIN:'', 
-    successMessage:'',
-    showPINInput: true, 
-    
-  }); 
-  watch(successMessage, () =>{
-      data.showPINInput = false; 
-      data.SuccessMessage= true; 
-      setTimeout(()=> {
-        data.showPINInput = true; 
-        data.showSuccessMessage = false; 
-      }, 20000);
-    })
-  function submitPIN() {
-    console.log('Entered submitPIN function');  
-    console.log('Button clicked');
-    console.log('Entered PIN type:', typeof data.enteredPIN); // Log entered PIN type for debugging
-    console.log('Correct PIN type:', typeof props.correctPIN);
-    console.log('Entered PIN:', data.enteredPIN); // Log entered PIN for debugging
-    console.log('Correct PIN:', props.correctPIN);
 
-    if  (data.enteredPIN === props.PIN) {
-      data.successMessage = 'Erfolgreiche PIN Eingabe';
-      console.log('Correct PIN entered!');
-        
+const props = defineProps({
+  user:Object, 
+  correctPIN: String
+
+});   
+const successMessage = ref('')
+const data= ref({
+  enteredPIN:'',
+  successMessage:'',
+  showPINInput:true
+  
+});
+
+watch(successMessage, () =>{
+  data.showPINInput = false; 
+  data.SuccessMessage= true; 
+  setTimeout(()=> {
+    data.showPINInput = true; 
+    data.showSuccessMessage = false; 
+  }, 20000);
+})
+ 
+function submitPIN() {
+  console.log('Entered submitPIN function');  
+  console.log('Button clicked');
+  console.log('Entered PIN type:', typeof data.enteredPIN); // Log entered PIN type for debugging
+  console.log('Correct PIN type:', typeof props.correctPIN);
+  console.log('Entered PIN:', data.enteredPIN); // Log entered PIN for debugging
+  console.log('Correct PIN:', props.correctPIN);
+
+
+  if  (data.enteredPIN === String(props.correctPIN)) {
+    successMessage.value = 'Erfolgreiche PIN Eingabe';
+    console.log('Correct PIN entered!');
+    
+  } else {
+    successMessage.value = 'Falsche PIN. Bitte nochmal versuchen';
+    console.log('Incorrect PIN entered.');
+  }
+  
+}
+
+</script>
+
+<template>
+  <v-card theme="dark">
+    <div class="d-flex flex-no-wrap justify-space-between">
+      <div>
+        <v-card-title class="text-h5">
+          {{ user.Vorname }} {{ user.Name }} 
+        </v-card-title>
+
+        <v-card-subtitle>{{ user.Mail }}</v-card-subtitle>
       
-    } else {
-      data.successMessage = 'Falsche PIN. Bitte nochmal versuchen';
-      console.log('Incorrect PIN entered.');
-    }
-    
-  }
-  </script>
+      <v-text-field
+        v-if="data.showPINInput"
+        v-model="data.enteredPIN"
 
-  <template>
-    <v-card theme="dark">
-      <div class="d-flex flex-no-wrap justify-space-between">
-        <div>
-          <v-card-title class="text-h5">
-            {{ user.Vorname }} {{ user.Name }} 
-          </v-card-title>
+        label="Enter PIN"
+        color="blue"
+        ></v-text-field>
 
-          <v-card-subtitle>{{ user.Mail }}</v-card-subtitle>
-        
-        <v-text-field
-          v-if="data.showPINInput"
-          v-model="data.enteredPIN"
-          label= "Enter PIN"
-          type="password"   
-          color="blue"
-          ></v-text-field> <!-- mit type password, wird die pin unsichtbar vice versa -->
 
-        <v-btn @click="submitPIN">PIN einsenden</v-btn>
-        <div v-if="data.successMessage"> {{ data.successMessage }}</div>
-        </div>
-
-        <v-avatar class="ma-3" size="125" rounded="0">
-          <v-img :src="'https://robohash.org/' + user.Name + '.png'"></v-img>
-        </v-avatar>
+      <v-btn @click="submitPIN">PIN einsenden</v-btn>
+      <div v-if="successMessage"> {{ successMessage }}</div>
       </div>
-    </v-card>
-  </template>
 
-  <style scoped>
-  p {
-    font-family: sans
-  }
-  </style>
+      <v-avatar class="ma-3" size="125" rounded="0">
+        <v-img :src="'https://robohash.org/' + user.Name + '.png'"></v-img>
+      </v-avatar>
+    </div>
+  </v-card>
+</template>
+
+<style scoped>
+p {
+  font-family: sans
+}
+</style>
