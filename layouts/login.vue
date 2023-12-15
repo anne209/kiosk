a4445b1a6d5e99e1c8a3bbfcf00ff6a9577e8380
anne209 <anne.denk@yahoo.de>
2023-12-15 21:49:28 +0100

    commit

--- layouts/login.vue
+++ layouts/login.vue
@@ -3,7 +3,15 @@
     <v-app id="inspire">
       <v-app-bar extended
       color="blue">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-btn @click="goToHomePage">
+
+          <v-icon
+          start
+          icon="mdi-arrow-left"
+        ></v-icon>
+
+          Zur Homepage gehen
+        </v-btn>
   
         <v-app-bar-title>Login Page für Admin und Users</v-app-bar-title>
   
@@ -18,3 +26,24 @@
       </v-main>
     </v-app>
   </template>
+
+
+<script setup>
+  
+import {ref} from 'vue'
+
+const drawer =ref(null)
+
+</script>
+
+
+<script>
+    export default {
+    data:()=>({drawer:null }), 
+      methods:{
+        goToHomePage(){
+          this.$router.push('/home')
+        }
+      }
+  }
+</script>
