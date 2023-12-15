69a753e4a5c56bfa2eaf187f723a89d805643a64
haukur1710 <73662487+haukur1710@users.noreply.github.com>
2023-12-15 13:43:06 +0100

    commit

--- components/user.vue
+++ components/user.vue
@@ -2,21 +2,37 @@
   const props = defineProps(['user','PIN']          // hier kommt wird die pin als prop übergeben 
     
   ); 
+  const successMessage =ref('')
   const data = ref({
     enteredPIN:'', 
-    showPINInput: true, 
     successMessage:'',
+    showPINInput: true, 
+    
   }); 
+  watch(successMessage, () =>{
+      data.showPINInput = false; 
+      data.SuccessMessage= true; 
+      setTimeout(()=> {
+        data.showPINInput = true; 
+        data.showSuccessMessage = false; 
+      }, 20000);
+    })
   function submitPIN() {
     console.log('Entered submitPIN function');  
     console.log('Button clicked');
+    console.log('Entered PIN type:', typeof data.enteredPIN); // Log entered PIN type for debugging
+    console.log('Correct PIN type:', typeof props.correctPIN);
     console.log('Entered PIN:', data.enteredPIN); // Log entered PIN for debugging
     console.log('Correct PIN:', props.correctPIN);
+
     if  (data.enteredPIN === props.PIN) {
       data.successMessage = 'Erfolgreiche PIN Eingabe';
+      console.log('Correct PIN entered!');
+        
       
     } else {
       data.successMessage = 'Falsche PIN. Bitte nochmal versuchen';
+      console.log('Incorrect PIN entered.');
     }
     
   }
@@ -36,9 +52,9 @@
           v-if="data.showPINInput"
           v-model="data.enteredPIN"
           label= "Enter PIN"
-          type="password"
+          type="password"   
           color="blue"
-          ></v-text-field>
+          ></v-text-field> <!-- mit type password, wird die pin unsichtbar vice versa -->
 
         <v-btn @click="submitPIN">PIN einsenden</v-btn>
         <div v-if="data.successMessage"> {{ data.successMessage }}</div>
