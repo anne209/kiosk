f6df322929ec3a1f6618e2456c6643a270fd11bf
haukur1710 <73662487+haukur1710@users.noreply.github.com>
2024-01-02 19:37:30 +0000

    commit

---
+++ components/transaktion.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+const props = defineProps({
+  transaktion:Object,
+  
+
+});   
+</script>
+<template>
+    <v-list>
+      <v-list-item-group>
+<v-list-item>
+  {{ transaktion.Personen }}
+</v-list-item>
+        <v-list-item>
+          <!-- Produkt -->
+          <v-list-item-content class="custom-product-quantity">
+            <v-list-item-title class="headline text--primary">
+              Bestellung {{ transaktion.Produkt }}
+            </v-list-item-title>
+            <v-list-item-subtitle class="caption text--secondary">
+              {{ transaktion.Anzahl }} Transaktionen
+            </v-list-item-subtitle>
+          </v-list-item-content>
+  
+          <!-- Abrechnungszeitpunkt -->
+          <v-list-item-content class="text right">
+            <v-list-item-title class="headline" v-if="transaktion.Abrechnungszeitpunkt">
+              <v-icon color ="success"> mdi-check-circle-outline </v-icon>
+              Abrechnung erfolgt am {{ transaktion.Abrechnungszeitpunkt }}
+            </v-list-item-title>
+            <v-list-item-title v-else class="headline">
+                <v-icon color="error"> mdi-alert-circle-outline </v-icon>
+                Abrechnung noch nicht erfolgt
+            </v-list-item-title>
+          </v-list-item-content>
+  
+          <!-- Transaktionszeitpunkt -->
+          <v-list-item-content>
+            <v-list-item-subtitle class="caption text--secondary">
+              {{ transaktion.Transaktionszeitpunkt }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+          <v-divider class="mt-2 mb-2"></v-divider>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </template>
+  
+  <style scoped>
+  .custom-product-quantity {
+    border-right: 1px solid v-app-bar-border; /* Border color */
+  }
+  </style>
--- pages/transaktionen.vue
+++ pages/transaktionen.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-definePageMeta({           // hier definieren wir das admin layout ansonsten wird immer default benutzt
-layout: 'admin'
+definePageMeta({layout: 'admin'});
+
+const { data: transaktionen, pending, error } = await useFetch(`http://localhost:8080/v1/graphql`, {
+  method: "POST",
+  body: { query: "query {swps_Transaktion {Abrechnungszeitpunkt Anzahl Transaktionszeitpunkt Transaktions_ID Personen { Name Personen_ID } Produkt { Name Preis} }}" },
 })
 </script>
 <template>
 
+<p v-if="pending">Fetching...</p>
+  <pre v-else-if="error">Could not load: {{ error.data }}</pre>
+  <div v-else>
+    <v-list>
+    <v-list-item v-for="transaktion in transaktionen.data.swps_Transaktion" :key="transaktion.Transaktions_ID">
+      <transaktion :transaktion="transaktion"> </transaktion>
 
-
-transaktionsübersicht
+    </v-list-item>
+  </v-list>  
+  </div>
 </template>
