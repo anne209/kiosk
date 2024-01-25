2ea383c8f1b4d41133c1cc1aa859c71d9b52e8d5
anne209 <anne.denk@yahoo.de>
2024-01-25 17:37:26 +0100

    commit

--- pages/transaktionen.vue
+++ pages/transaktionen.vue
@@ -5,7 +5,54 @@ const { data: transaktionen, pending, error } = await useFetch(`http://localhost
   method: "POST",
   body: { query: "query {swps_Transaktion {Abrechnungszeitpunkt Anzahl Transaktionszeitpunkt Transaktions_ID Personen { Name Personen_ID } Produkt { Name Preis} }}" },
 })
+
+
+
+const { graphQlQueryToJson } = require("graphql-query-to-json");
+
+const { data: personen, pending: personenPending } = await useFetch(`http://localhost:8080/v1/graphql`, {
+  method: "POST",
+  body: JSON.stringify({ query: "query { swps_Personen { Vorname Name }}" }),
+});
+
+
+const Vorname = ref('');
+const Name = ref('');
+
+// Corrected the query string, added 'query' keyword, and fixed syntax for variables
+const query = `
+  query viewer($Vorname: String, $Name: String) {
+    viewer {
+      personal(criteria: { Vorname: $Vorname, Name: $Name }) {
+        Vorname
+        Name
+      }
+    }
+  }
+`;
+
+// Access the value property of reactive variables for the 'variables' object
+const result = graphQlQueryToJson(query, {
+  variables: {
+    Vorname: Vorname.value,
+    Name: Name.value,
+  },
+});
+
+const jsonString = JSON.stringify(result);
+const fs = require ('fs');
+const filePath = 'C:/Users/user/kiosk/result.json';
+fs.writeFileSync(filePath, jsonString);
+
+console.log(personen);
+
+
+
 </script>
+
+
+
+
 <template>
 
 <p v-if="pending">Fetching...</p>
