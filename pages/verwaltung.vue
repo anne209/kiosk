a02cf62dd6d8b732ace5b996ea92b979803c9aa1
anne209 <anne.denk@yahoo.de>
2024-02-07 23:10:08 +0100

    commit

--- pages/verwaltung.vue
+++ pages/verwaltung.vue
@@ -6,17 +6,21 @@ layout: 'admin'
 <template>
 
  <v-container>
-      <v-row> 
-        <v-col cols="12" md="6">
-
-      <newproduct></newproduct>
-      <newlocation></newlocation>
-      
-      </v-col> <v-col cols="12" md="6">
-      <newuser></newuser>
-     
-    </v-col>
-  </v-row>
+  <v-row>
+      <v-col cols="12" md="6">
+        <v-row>
+          <v-col cols="12">
+            <newproduct></newproduct>
+          </v-col>
+          <v-col cols="12">
+            <newlocation></newlocation>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="12" md="6">
+        <newuser></newuser>
+      </v-col>
+    </v-row>
 
     </v-container>
 </template>
