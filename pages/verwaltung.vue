aadfb4eb9e43cc0f1d18164db3fe459627ac7a25
anne209 <anne.denk@yahoo.de>
2024-02-07 23:30:46 +0100

    commit

--- pages/verwaltung.vue
+++ pages/verwaltung.vue
@@ -11,7 +11,7 @@ layout: 'admin'
   <v-row>
    <h3 class="ml-3"> Schnellzugriff </h3>
  </v-row>
-  <v-row>
+  <v-row> <!-- zweimal row und col, damit newproduct und newlocation links untereinander stehen und newuser rechts alleine steht -->
       <v-col cols="12" md="6">
         <v-row>
           <v-col cols="12">
