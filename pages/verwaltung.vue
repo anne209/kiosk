4381d75ce0c8b52eee478f21aba926bf6060995b
haukur1710 <73662487+haukur1710@users.noreply.github.com>
2024-02-07 23:01:03 +0100

    commit

--- pages/verwaltung.vue
+++ pages/verwaltung.vue
@@ -4,11 +4,16 @@ layout: 'admin'
 })
 </script>
 <template>
+
  <v-container>
+  <v-row>
+    <h3 class="ml-3"> Schnellzugriff </h3>
+  </v-row>
       <v-row> 
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="6" >
 
       <newproduct></newproduct>
+      
       <newlocation></newlocation>
       
       </v-col> <v-col cols="12" md="6">
