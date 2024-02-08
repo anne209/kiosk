472a1fe7d86bc2a43243726e2013f983c9e0825d
haukur1710 <73662487+haukur1710@users.noreply.github.com>
2024-02-09 00:20:32 +0100

    commit

--- components/useradmin.vue
+++ components/useradmin.vue
@@ -1,74 +1,76 @@
 <script setup>
-import {ref} from 'vue'; 
-import LionScratchIcon from "@/components/LionScratchIcon.vue";
+  import {ref} from 'vue'; 
+  import LionScratchIcon from "@/components/LionScratchIcon.vue";
 
-// LöwenScratch bearbeiten 
-const iconColor = ref('silver'); // die Farbe wird hier nicht verändert, ist fest eingstellt in LionScratchIcon.vue
-const iconWidth = ref('70'); // Breite
-const iconHeight = ref('80'); // Höhe
+  // LöwenScratch bearbeiten 
+  const iconColor = ref('silver'); //Farbe 
+  const iconWidth = ref('70'); //Breite
+  const iconHeight = ref('80'); //Höhe
 
-
-const props = defineProps({
-    user:Object, 
-}); 
+  // User Daten als props aus Nutzerverwaltung 
+  const props = defineProps({
+      user:Object, 
+  }); 
 
 </script>
 
 
 <template>
-    <v-card class="card" elevation="9" max-width="400" hover >
-     
-        
-      <v-card-item class="py-0"> 
+  <!-- Useradmin card-->
+  <v-card class="card" elevation="9" max-width="400" hover >
+
+    <v-card-item class="py-0"> 
         <v-card-title class="text-h6 d-flex align-center" style="flex: 1;">
             {{ user.Vorname }}
             {{ user.Name }} 
-           <div class="ml-auto">
+              <div class="ml-auto">
                 <LionScratchIcon :color="iconColor" :width="iconWidth" :height="iconHeight" />
               </div>
         </v-card-title>
-        <v-card-subtitle>{{ user.Mail }}</v-card-subtitle>
-      <v-card-text>
-        <div>
+
+        <v-card-subtitle> Mail: {{ user.Mail }}</v-card-subtitle>
+        <v-card-subtitle>ID: {{ user.Personen_ID }}</v-card-subtitle>
+
+        <v-card-text>
           Aktiv: {{ user.Aktiv }}
-        <div>
+          <div>
           Standort: {{ user.Standort.Name}}
-        </div>
-        </div>
-        <div> 
-         Anzahl der Transaktionen: {{ user.Transaktions_aggregate.aggregate.count}}
-        </div>
-        <!-- hier müsste noch die richige query aus dem parent -->
-        <div>
-          Größte einmalige Bestellung: {{ user.Transaktions[0]?.Anzahl }} - {{ user.Transaktions[0]?.Produkt?.Name }} ({{ user.Transaktions[0]?.Produkt?.Produkt_ID }})
-        </div>
-        <div>
-          Letztes Update: {{ user.PersonenExt.Latest_update }}
-        </div>
+          </div>
+
+          <div> 
+          Anzahl der Transaktionen: {{ user.Transaktions_aggregate.aggregate.count}}
+          </div>
+          
+          <div>
+            Anzahl gekaufter Produke: {{ user.Transaktions_aggregate?.aggregate.sum?.Anzahl }}
+          </div>
+
+          <div>
+            Größte Bestellung: {{ user.Transaktions[0]?.Anzahl }} - {{ user.Transaktions[0]?.Produkt?.Name }} 
+          </div>
+
+          <div>
+            Letztes Update: {{ user.PersonenExt.Latest_update }}
+          </div>
          
-         <!-- hier könnte noch das Lieblingsprodukt stehen-->
-         <!-- Nutzer Standort anzeigen -->
         </v-card-text>
-        
-     </v-card-item>
-   </v-card>
+    </v-card-item>
+  </v-card>
 </template>
 
 <style scoped>
+
+/*  Diverse Stylings für Usercard */
 .card {
     
     border-radius: 18px; 
-    box-shadow: 0px 4px 24px 2px rgba(255, 255, 255, 0.2); /* Adjust spread and blur */
+    box-shadow: 0px 4px 24px 2px rgba(255, 255, 255, 0.2); 
     backdrop-filter: blur(20px);
     
-
-    /*  Hier muss weiss schwarz swerden */
     background: rgb(2,0,3);
     background: linear-gradient(70deg, rgba(2,0,3,1) 60%, rgba(2,2,6,1) 67%, rgba(2,49,68,1) 77%, rgba(1,78,107,1) 82%, rgba(1,107,147,1) 87%, rgba(0,148,202,1) 92%, rgba(0,205,181,1) 97%, rgba(0,233,171,1) 100%);
     
-    /* Other styles for text, etc. inside the card */
-    color: #FFF; /* For text color, assuming a light background */
-    font-family: 'Arial', sans-serif; /* Font style for text */
+    color: #FFF; /* Textfarbe */
   }
 
 </style>
--- pages/nutzerverwaltung.vue
+++ pages/nutzerverwaltung.vue
@@ -1,4 +1,4 @@
-<!-- einfach mehr sortierungen und lieblingsprodukt muss irgendwie gefunden werden -->
+
 <script setup>
   definePageMeta({layout: 'admin'});
   import { ref, onMounted } from 'vue';
@@ -6,19 +6,21 @@
   
   const error = ref(''); 
   const pending = ref(''); 
-  const selectedSorting = ref('Latest_update_DESC'); // Default sorting criteria
+  const selectedSorting = ref('Latest_update_DESC'); //standard Sortierung 
   const users = ref('');
   const sortingOptions = ref([
-    'Transaktions_aggregate.count_ASC',
-    'Transaktions_aggregate.count_DESC',
+    'Meiste Transaktionen',
+    'Wenigste Transaktionen',
     'Latest_update_ASC',
     'Latest_update_DESC',
     'Standort_ASC',
     'Standort_DESC', 
+    'Anzahl gekaufter Produkt_DESC', 
+    'Anzahl gekaufter Produkte_ASC', 
     // Hier können weiter Filter hinzugefügt werden 
-    // Hier müsste man das noch übersetzen 
   ]);
   
+  // hier werden die User gefetched anhand der Sortiervariable
   const fetchData = async () => {
     try {
       console.log('Fetching data...');
@@ -49,6 +51,9 @@
                 Transaktions_aggregate {
                   aggregate {
                     count
+                    sum{
+                      Anzahl
+                    }
                   }
                 }
                 PersonenExt {
@@ -58,7 +63,7 @@
             }
           `,
           variables: {
-            order_By: parseSorting(selectedSorting.value), // Funktion für das Parsen der Filter Variablen 
+            order_By: parseSorting(selectedSorting.value), // Sortiervariable 
           },
         }),
       });
@@ -81,12 +86,12 @@
   
   // Hier werden die verschiedenen Filter durchgegangen 
   function parseSorting(selectedSorting) {
-    // Hier wird bei der Auswahl der Filter, das Format passend für die Variable in GraphQL Hasura angepasst 
 
+  // Hier wird bei der Auswahl der Filter, das Format passend für die Variable in GraphQL Hasura angepasst 
 
-    if (selectedSorting === 'Transaktions_aggregate.count_ASC') {
+    if (selectedSorting === 'Wenigste Transaktionen') {
       return [{ Transaktions_aggregate: { count: 'asc'  } }];
-    } else if (selectedSorting === 'Transaktions_aggregate.count_DESC') {
+    } else if (selectedSorting === 'Meiste Transaktionen') {
       return [{ Transaktions_aggregate: { count: 'desc' } }];
     } else if (selectedSorting === 'Latest_update_ASC') {
       return [{ PersonenExt: { Latest_update: 'asc' } }];
@@ -96,9 +101,13 @@
       return [{ Standort: { Name: 'asc'} }];
     } else if (selectedSorting === 'Standort_DESC') {
       return [{ Standort: { Name: 'desc'} }];
+    } else if (selectedSorting === 'Anzahl gekaufter Produkt_DESC') {
+      return [{ Transaktions_aggregate: { sum:{Anzahl: 'desc'}} }];
+    } else if (selectedSorting === 'Anzahl gekaufter Produkte_ASC') {
+      return [{ Transaktions_aggregate: { sum:{Anzahl: 'asc'}}  }];
       
     } else {
-      // Hier können weiter filter gehandlet werden 
+      // Hier können weiter filter gehandlet werden siehe sortingOptions  
       return [];
     }
   }
@@ -108,6 +117,7 @@
   });
 
 
+// Alles für den Excelexport
 
   // Excelfunction 
   const excelfunction = async () => {
@@ -121,7 +131,7 @@
             return;
         }
 
-    // dataToExport wird mit den daten aus der query definiert
+  // dataToExport wird mit den daten aus der query definiert
     const dataToExport = users.value.swps_Personen;
 
     if (!Array.isArray(dataToExport) || dataToExport.length === 0) {
@@ -131,9 +141,9 @@
 
   console.log(users.value)
 
-  const wb = XLSX.utils.book_new(); 
-
   // Daten für Export vorbereiten 
+
+  const wb = XLSX.utils.book_new(); 
   
   const ws = XLSX.utils.json_to_sheet(dataToExport.map(tx => ({
     'Personen-ID': tx.Personen_ID,
@@ -142,6 +152,7 @@
     'E-Mail': tx.Mail,
     'Standort': tx.Standort?.Name,
     'Anzahl Transaktionen': tx.Transaktions_aggregate?.aggregate.count,
+    'Anzahl gekaufter Produkte':tx.Transaktions_aggregate?.aggregate.sum.Anzahl, 
     'Grösste einmalige Bestellung': tx.Transaktions?.[0] && `${tx.Transaktions[0].Anzahl} - ${tx.Transaktions[0].Produkt.Name} (${tx.Transaktions[0].Produkt.Produkt_ID})` || 'N/A',
     'Latest_update': tx.PersonenExt?.Latest_update,
   })));
@@ -156,7 +167,7 @@
     }
     const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
 
-    // Trigger den Daten download
+    // Datendownload triggern
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
     link.download = 'User.xlsx';
@@ -172,31 +183,34 @@
   <v-container>
 
     <v-row>
-    <h3 class="ml-3"> Userverwaltung </h3>
+    <h3 class="ml-3">Userverwaltung</h3>
     </v-row>
      <v-row>
-      <v-col cols="6" >
-      <!-- Hier ist das autocomplete für die filter auswahl-->
-      <v-autocomplete
-        v-model="selectedSorting"
-        :items="sortingOptions"
-        label="Nutzer sortieren nach"
-        @update:model-value="fetchData"
-        prepend-inner-icon="mdi-sort"
-        variant="underlined"
-      ></v-autocomplete>
+      <v-col cols="6">
+
+      <!-- Hier ist das autocomplete für die Sortierauswahl-->
+        <v-autocomplete
+          v-model="selectedSorting"
+          :items="sortingOptions"
+          label="Nutzer sortieren nach"
+          @update:model-value="fetchData"
+          prepend-inner-icon="mdi-sort"
+          variant="underlined"
+        ></v-autocomplete>
        </v-col>
+
+       <!-- excel btn-->
       <v-col cols="6" >
-      <v-btn
-      @click="excelfunction"
-      size="x-large"
-      block
-      color="success"
-      append-icon="mdi-table-arrow-up"
-      >Excel Datei exportieren
+        <v-btn
+          @click="excelfunction"
+          size="x-large"
+          block
+          color="success"
+          append-icon="mdi-table-arrow-up"
+          >Excel Datei exportieren
       </v-btn>
      
-    </v-col>
+      </v-col>
     </v-row>
   </v-container>
   
